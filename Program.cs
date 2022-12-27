91057a6d8973639dd308f33a43bf4655a192af2b
/dev/null <a.d.kamenev@yandex.ru>
2022-12-27 21:54:39 +0300

    added main method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using FinProjectBaseDev;
+
+string[] havingArray = {"srtu", "uiy", "oiuy", "oiuo", "uiyrer", "d", "", "ad"};
+
+int lengthLimit = 3;
+Console.WriteLine();
+string[] resultArray = FindShortStrings(havingArray, lengthLimit);
+
+
+string[] FindShortStrings (string[] startArray, int lengthLimit){
+    StringArr tempStringArr = new StringArr(0);
+    for (int i = 0; i < startArray.Length; i++){
+        if ((startArray[i]).Length <= lengthLimit){
+            tempStringArr.Add(startArray[i]);
+        }
+    }
+    string[] res = tempStringArr.Pack;
+    return res;
+}
+
