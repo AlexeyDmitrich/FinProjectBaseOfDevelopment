c91746c4175143ab3a703b1f34ce3d2483d06242
/dev/null <a.d.kamenev@yandex.ru>
2022-12-29 18:05:30 +0300

    added comments

--- Program.cs
+++ Program.cs
@@ -7,6 +7,13 @@ Console.WriteLine();
 string[] resultArray = FindShortStrings(havingArray, lengthLimit);
 PrintString(resultArray);
 
+/// <summary>
+/// метод собирает из массива строки 3 и менее символов длиной и собирает в новый массив
+/// </summary>
+/// <param name="startArray">массив строк</param>
+/// <param name="lengthLimit">ограничения длины строк для сборки нового массива</param>
+/// <returns>Метод вернёт массив из строк с ограниченной длинной</returns>
+
 string[] FindShortStrings (string[] startArray, int lengthLimit){
     StringArr tempStringArr = new StringArr();
     for (int i = 0; i < startArray.Length; i++){
@@ -19,6 +26,10 @@ string[] FindShortStrings (string[] startArray, int lengthLimit){
     return res;
 }
 
+/// <summary>
+/// метод для вывода элементов массива в консоль
+/// </summary>
+/// <param name="array">массив строк</param>
 void PrintString (string[] array){
     for(int i = 0; i < array.Length; i++){
         Console.Write($"{array[i]} ");
